ab81b9b6f06afcfac163fe8ecd5504d0fa8dc4d0
CangShui <s8s@live.com>
2021-03-17 10:33:26 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import Link from './link';
 
-const Footer = () => {
-  return (
-    <div id="footer">
-      <div className="container">
-        <p></p>
-        <p></p>
-      </div>
-    </div>
-  );
-}
 
 export default Footer;
