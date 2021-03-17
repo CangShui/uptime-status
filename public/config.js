658eab692e7a43dcd63c61b82ef8a431edf1d751
CangShui <s8s@live.com>
2021-03-17 09:41:43 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -30,12 +30,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'Blog',
+      url: 'https://cangshui.net
     }
   ]
 };
